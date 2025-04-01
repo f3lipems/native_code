fad7a643ab3c5a1d4850e5a74cdb0f9e38505829
Felipe <felipe@visionar.io>
2025-04-01 16:34:06 -0300

    add method channel in kotlin

--- android/app/src/main/kotlin/com/example/native_code/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/native_code/MainActivity.kt
@@ -1,5 +1,25 @@
 package com.example.native_code
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+    private val CHANNEL = "visionar.io/native_code"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method == "calcSum") {
+                val a = call.argument<Int>("a") ?: 0
+                val b = call.argument<Int>("b") ?: 0
+                result.success(a + b)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+}
